190cd59389330a54a0f67f630656142d7aa689d6
Alexander Bech <alexanderlillevangbech@gmail.com>
2021-12-01 07:32:23 +0100

    cleaning

--- 2021/1/puzzle2.cpp
+++ 2021/1/puzzle2.cpp
@@ -7,18 +7,14 @@ using namespace std;
 
 int main() 
 {
-//  	auto lol = read_input();
-//  	for (auto s : lol) cout << s << std::endl;
-//  	return 0;
 	int first, second, third;
 	int temp;
 	int current_sum, last_sum;
 	cin >> first >> second >> third ;
 	int counti = 0;
-	while (!cin.eof())
+	while (!_EOF())
 	{
 		cin >> temp;
-		cout << temp << "\n";
 		last_sum = first + second + third;
 		current_sum = second + third + temp;
 
@@ -29,5 +25,5 @@ int main()
 		second = third;
 		third = temp;
 	}
-	cout << counti;
+	cout << counti << "\n";
 }
--- 2021/util.h
+++ 2021/util.h
@@ -1,3 +1,5 @@
+inline bool _EOF() {return (std::cin >> std::ws).eof(); }
+
 inline std::vector<std::string> read_input()
 {
 	std::string line;
@@ -5,6 +7,7 @@ inline std::vector<std::string> read_input()
 	while (std::getline(std::cin, line)) v.push_back(line);
 	return v;
 }
+
 
 inline void findPermutations(std::string str, int leftIndex, int rightIndex, std::vector<std::string>& v)
 {
