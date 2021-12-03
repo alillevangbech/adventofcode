2c947b13077f059f5043d059895e71e27cfcd482
Alexander Bech <alexanderlillevangbech@gmail.com>
2021-12-03 11:03:55 +0100

    day 3

--- 2021/3/puzzle1.cpp
+++ 2021/3/puzzle1.cpp
@@ -1,43 +1,25 @@
-#include <algorithm>
 #include <bits/stdc++.h>
-#include <cstdlib>
-#include <iterator>
 #include "../util.h"
+#define bits bitset<32>
 using namespace std;
-#define n 12
-#define bits bitset<n>
 int main() 
 {
 	auto input = read_input();
-
-	vector<int> v(n, 0);
+	int cb,oz,n = input[0].size();
+	vector<int> ones(n, 0), zeros(n, 0);
+	vector<bool> tmp;
 	vector<bits> b;
-	transform(begin(input), end(input), back_inserter(b),[](const string& s)
-		{
-			return bits(s);
-		});
-	
-	for (auto x : b) {
+	transform(begin(input), end(input), back_inserter(b),[](const string& s){ return bits(s); });
+	for (auto x : b) 
 		for (int i = 0; i < n; i++)
-		{
-			v[i] += x[i];
-		}
-	}
-
-
-	vector<bool> bi1;
-	bits bi2;
-	int threshold = input.size() / 2;
-	transform(begin(v),end(v),back_inserter(bi1),
-		[&](int i) 
-		{
-			return i > threshold;
-		});
-
-	for (int i = 0; i < n; i++) {
-		bi2[i] = bi1[i];
-	}
-	cout << bi2.to_ulong() * bi2.flip().to_ulong() << endl;
-
+			if (x[i])
+				ones[i]++;
+			else
+				zeros[i]++;
+	transform(begin(ones), end(ones), begin(zeros), back_inserter(tmp), greater_equal<int>{});
+	cb = accumulate(rbegin(tmp), rend(tmp), 0, [](int x, int y) {return (x << 1) + y; });
+	tmp.flip();
+	oz = accumulate(rbegin(tmp), rend(tmp), 0, [](int x, int y) {return (x << 1) + y; });
+	cout << cb * oz << endl;
 	return 0;
 }
--- 2021/3/puzzle2.cpp
+++ 2021/3/puzzle2.cpp
@@ -1,73 +1,28 @@
-#include <algorithm>
 #include <bits/stdc++.h>
-#include <cstdlib>
-#include <iterator>
 #include "../util.h"
+#define bits bitset<32>
 using namespace std;
-#define n 12
-#define bits bitset<n>
+unsigned fun(vector<bits> b, bool bit, int n)
+{
+	vector<int> o(n,0), z(n,0);
+	int cur_bit = n - 1;
+	auto common = [&]() {return bit ? o[cur_bit] >= z[cur_bit] : o[cur_bit] < z[cur_bit];};
+	while (b.size() != 1) { 
+		for_each(b.begin(), b.end(), [&](const bits& bz) { if (bz[cur_bit]) o[cur_bit]++; else z[cur_bit]++;});
+		b.erase(remove_if(begin(b),end(b),[&](const bits& bz) {return bz[cur_bit] == !common();}),end(b));
+		cur_bit--;
+	}
+	return b[0].to_ulong();
+}
+
 int main() 
 {
 	auto input = read_input();
-	double threshold = input.size() / 2.0;
-	int length = input.size();
-	vector<int> v(n, 0);
+	unsigned cb,oz,n = input[0].size();
 	vector<bits> b;
-	transform(begin(input), end(input), back_inserter(b),[](const string& s)
-		{
-			return bits(s);
-		});
-	
-	auto thresh = [](int i, double threshold) { return i >= threshold;};
-	vector<bits> tmp1, tmp2;
-
-	bool val = 0;
-	tmp1 = b;
-	int cur_bit = n - 1;
-	while (tmp1.size() != 1) {
-		threshold = tmp1.size() / 2.0;
-		cout << threshold << "-" << tmp1.size() << endl;
-		for (auto i = 0; i < tmp1.size(); i++) {
-			v[cur_bit] += tmp1[i][cur_bit];
-		}
-		val = thresh(v[cur_bit],threshold);
-		cout << v[cur_bit] << ";" << val << endl;
-		for (auto i = 0; i < tmp1.size(); i++) {
-			if (tmp1[i][cur_bit] == val)
-				tmp2.push_back(tmp1[i]); 
-		}
-		tmp1 = tmp2;
-		tmp2.clear();
-		cur_bit--;
-	}
-	int first = tmp1[0].to_ulong(); 
-	
-	cout << endl;
-	val = 0;
-	tmp1 = b;
-	cur_bit = n - 1;
-	tmp1 = b;
-	tmp2.clear();
-	fill(begin(v),end(v),0);
-	while (tmp1.size() != 1) {
-		threshold = tmp1.size() / 2.0;
-		cout << threshold << "-" << tmp1.size() << endl;
-		for (auto i = 0; i < tmp1.size(); i++) {
-			v[cur_bit] += tmp1[i][cur_bit];
-		}
-		val = !thresh(v[cur_bit],threshold);
-		cout << v[cur_bit] << ";" << val << endl;
-		for (auto i = 0; i < tmp1.size(); i++) {
-			if (tmp1[i][cur_bit] == val)
-				tmp2.push_back(tmp1[i]); 
-		}
-		tmp1 = tmp2;
-		tmp2.clear();
-		cur_bit--;
-	}
-	cout << tmp1[0] << endl;
-	int second = tmp1[0].to_ulong(); 
-	cout << second * first << endl;
-
+	transform(begin(input), end(input), back_inserter(b),[](const string& s){ return bits(s); });
+	oz = fun(b,1,n);
+	cb = fun(b,0,n);
+	cout << cb * oz << endl;
 	return 0;
 }
