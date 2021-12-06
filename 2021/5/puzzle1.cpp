1cab954f97fa27394649006c760e1c4c7757e975
Alexander Bech <alexanderlillevangbech@gmail.com>
2021-12-06 16:27:25 +0100

    day 5 cleaner

--- 2021/5/puzzle1.cpp
+++ 2021/5/puzzle1.cpp
@@ -1,78 +1,30 @@
 #include <bits/stdc++.h>
-#include <cmath>
-#include <cstdio>
 #include "../util.h"
 using namespace std;
 #define n 1000
-struct vent {
-	int x1,x2,y1,y2;
-	vent(int x1, int y1, int x2, int y2) : x1(x1), y1(y1), x2(x2), y2(y2) {};
-	void print () {
-	cout << x1 << ";" << y1 << ";" << x2 << ";" << y2 << endl;
-	};
-};
-
-double slope(vent& v) {
-	double delta_x = (double)v.x2 - (double)v.x1;
-	double delta_y = (double)v.y1 - (double)v.y2;
-	double radian = atan2(delta_y,delta_x);
-	double degree = round(57.2957795 * radian);
-	degree += degree < 0 ? 360 : 0;
-	return degree;
-}
-
-int main() {
-	int ret;
-	int x1,x2,y1,y2;
-	vector<vent> s;
-	vector<vector<int>> map{n,vector<int>(n,0)};
-	while ((ret = scanf("%d %d %d %d\n", &x1, &y1, &x2, &y2)) != EOF) {
-		assert(ret == 4);
-		s.push_back(vent{x1,y1,x2,y2});
+static int umap[n][n];
+int main() 
+{
+	int stepx, stepy;
+	int x,y,w,v;
+	int i,j;
+	int sum = 0;
+	while (scanf("%d,%d -> %d,%d\n", &x, &y, &w, &v) != EOF) {
+		if (x == w || y == v) {
+			stepx = x < w ? 1 : -1;
+			stepy = y < v ? 1 : -1;
+			for (i = y, j = x; i != v || j != w; i += (i != v ? stepy : 0), j += (j != w) ? stepx : 0)
+				umap[i][j]++;
+			umap[i][j]++;
+		}
 	}
-
-	for (auto& v : s) {
-		int degree = slope(v);
-		if (!(v.x1 == v.x2 || v.y1 == v.y2)) continue;
-
-		switch (degree) {
-			case 0:
-				for (int i = v.x1; i <= v.x2; i++) map[v.y1][i]++; 
-				break;
-			case 45:
-				for (int x = v.x1, y = v.y1; x <= v.x2 && y >= v.y2; x++,y--) map[y][x]++; 
-				break;
-			case 90:
-				for (int i = v.y1; i >= v.y2; i--) map[i][v.x1]++; 
-				break;
-			case 135:
-				for (int x = v.x1, y = v.y1; x >= v.x2 && y >= v.y2; x--,y--) map[y][x]++; 
-				break;
-			case 180:
-				for (int i = v.x1; i >= v.x2; i--) map[v.y1][i]++; 
-				break;
-			case 225:
-				for (int x = v.x1, y = v.y1; x >= v.x2 && y <= v.y2; x--,y++) map[y][x]++; 
-				break;
-			case 270:
-				for (int i = v.y1; i <= v.y2; i++) map[i][v.x1]++; 
-				break;
-			case 315:
-				for (int x = v.x1, y = v.y1; x <= v.x2 && y <= v.y2; x++,y++) map[y][x]++; 
-				break;
-			default:
-				break;
-		};
-	}
-
-	int count = 0;
-	for_each(begin(map), end(map), [&](auto& v)
+	std::for_each(umap, umap + n, [&](const auto& m)
 	{
-		count += std::count_if(begin(v),end(v),[&](auto& vv)
+		std::for_each(m, m + n, [&](const auto& _m)
 		{
-			return vv > 1;
+			sum += _m > 1;
 		});
 	});
-	cout << count << endl;
-
+	cout << sum << endl;
+	return 0;
 }
