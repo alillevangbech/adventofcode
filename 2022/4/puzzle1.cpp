5898d183809920c765ff4b0b0e89de413e7aa60a
Alexander Bech <alexanderlillevangbech@gmail.com>
2022-12-04 06:32:48 +0100

    day 4

---
+++ 2022/4/puzzle1.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <ctype.h>
+#include <iostream>
+#include <math.h>
+#include <numeric>
+#include <ranges>
+#include <unordered_map>
+#include "../util.h"
+using namespace std;
+using elf = std::tuple<int,int,int,int>;
+
+int main() {
+	std::string line;
+	std::vector<elf> vec;
+	while (std::getline(std::cin, line)) {
+		int e1, e2, e3, e4;
+		sscanf(line.c_str(), "%d-%d,%d-%d", &e1,&e2,&e3,&e4); 
+		vec.push_back(tie(e1,e2,e3,e4));
+	}
+	int contained = 0;
+	for (auto [e1,e2,e3,e4] : vec)
+		if ((e1 <= e3 && e4 <= e2) || e3 <= e1 && e2 <= e4 )
+			contained++;
+	cout << contained << endl;
+}
